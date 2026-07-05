f4b170c75d0c95e910a8365b847b1048cd007e83
kingj1 <agallie962@gmail.com>
2026-07-05 02:15:22 -0500

    fixing  mobile response

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -78,18 +78,17 @@ export default function Home() {
     <>
       <section className="hero">
         <div className="hero-media">
-  <video
-    className="hero-video"
-    autoPlay
-    muted
-    loop
-    playsInline
-    preload="auto"
-  
-  >
-    <source src={heroVideo} type="video/mp4" />
-    Your browser does not support the video tag.
-        </video>
+ <video
+  className="hero-video"
+  autoPlay
+  muted
+  loop
+  playsInline
+  preload="auto"
+  poster={heroImage}
+>
+  <source src={electricVideo} type="video/mp4" />
+</video>
         </div>
 
         <div className="container hero-content">
